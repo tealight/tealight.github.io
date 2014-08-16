2a330b770b264671601c0f627a3060aa40fb4fd0
Ian Davies <git@iandavies.org>
2014-08-16 14:56:39 +0100

    Explictly name imported github modules.

--- app/js/app/skulpt-modules/tealight/utils.js
+++ app/js/app/skulpt-modules/tealight/utils.js
@@ -7,7 +7,7 @@ var $builtinmodule = function(name)
 		while (Date.now() < startTime + milliseconds.v) { /* spin */ }
     });
 
-    mod.github_load = new Sk.builtin.func(function(repo_owner, path) {
+    mod.github_load = new Sk.builtin.func(function(repo_owner, path, name) {
 		var http = new XMLHttpRequest();
 		var url = "https://api.github.com/repos/" + repo_owner.v + "/tealight-files/contents/" + path.v + "?access_token=" + params.githubToken;
 
@@ -17,7 +17,7 @@ var $builtinmodule = function(name)
 		if (http.status == 200) {
 			var py = atob(JSON.parse(http.responseText).content.replace("\n", ""));
 
-			Sk.importModuleInternal_("lib", false, undefined, py);
+			Sk.importModuleInternal_(name.v, false, undefined, py);
 		} else {
 			throw new Sk.builtin.Exception("Could not load " + path.v + " from Github user '" + repo_owner.v + "'");
 		}
