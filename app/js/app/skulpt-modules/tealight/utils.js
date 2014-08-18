a54141c273862e0c4e1dad3fc38593e2bb4b76df
Ian Davies <git@iandavies.org>
2014-08-18 18:19:14 +0100

    Implement now() and age() functions.

--- app/js/app/skulpt-modules/tealight/utils.js
+++ app/js/app/skulpt-modules/tealight/utils.js
@@ -1,11 +1,20 @@
 var $builtinmodule = function(name)
 {
     var mod = {};
+    var startTime = Date.now();
 	
     mod.sleep = new Sk.builtin.func(function(milliseconds) { // milliseconds will be a Sk.builtin.nmbr
 		var startTime = Date.now();
 		while (Date.now() < startTime + milliseconds.v) { /* spin */ }
     });
+
+    mod.now = new Sk.builtin.func(function() {
+    	return new Sk.builtin.nmber(Date.now() / 1000.0);
+    })
+
+    mod.age = new Sk.builtin.func(function() {
+    	return new Sk.builtin.nmber((Date.now() - startTime) / 1000.0)
+    })
 	
     return mod;
 }
