3252359157b83365d7685305d5242e2c02adb067
Ian Davies <git@iandavies.org>
2014-08-20 15:10:22 +0100

    Update indentation guides.

--- app/js/app/directives.js
+++ app/js/app/directives.js
@@ -3,107 +3,163 @@ define(["angular", "app/filters", "codemirrorPython"], function() {
 
 	/* CodeMirror addins */
 
-	CodeMirror.defineOption("scopeBG", false, function(cm, val, old) {
-		if (old && old != CodeMirror.Init) {
-			clearScopeBG(cm);
-			cm.off("renderLine", refreshScopeBG);
-			cm.off("change", refreshScopeBG);
-		}
-		if (val) {
-			setScopeBG(cm);
-			cm.on("refresh", refreshScopeBG);
-			cm.on("change", refreshScopeBG);
-		}
-	});
+	CodeMirror.defineOption("pythonIndentationGuides", false, function(cm, val, old) {
+        if (old && old != CodeMirror.Init) {
+            clearPythonIndendationGuides(cm);
+            cm.off("update", refreshPythonIndendationGuides);
+            cm.off("change", refreshPythonIndendationGuides);
+        }
+        if (val) {
+            createPythonIndendationGuides(cm);
+            cm.on("update", refreshPythonIndendationGuides);
+            cm.on("change", refreshPythonIndendationGuides);
+        }
+    });
 
-	function clearScopeBG(cm) {
-		for (var i = cm.display.lineSpace.childNodes.length - 1; i >= 0; i--) {
-			var node = cm.display.lineSpace.childNodes[i];
-			if (/(^|\s)scope-bg($|\s)/.test(node.className))
-				node.parentNode.removeChild(node);
-		}
-	}
+    function clearPythonIndendationGuides(cm) {
+        // Remove all children with class "cm-python-indent-guide"
+        for (var i = cm.display.lineSpace.childNodes.length - 1; i >= 0; i--) {
+            var node = cm.display.lineSpace.childNodes[i];
+            if (/(^|\s)cm-python-indent-guide($|\s)/.test(node.className))
+                node.parentNode.removeChild(node);
+        }
+    }
 
-	function setScopeBG(cm) {
-		var indents = [];
-		var parens = 0;
-		cm.eachLine(function(line) {
-			if (/^\s*$/.test(line.text)) {
-				indents.push(null)
-			} else {
-				if (parens > 0) {
-					indents.push(indents[indents.length-1])
-				} else {
-					indents.push(/^\s*/.exec(line.text)[0].length)
-				}
+    function createPythonIndendationGuides(cm) {
+        // Calculate indentation of each line
+        var indents = [];
+        var parens = 0;
+        var ml_string = false;
+        cm.eachLine(function(line) {
+            if (/^\s*$/.test(line.text)) {
+                // Empty lines don't contribute to indentation changes
+                indents.push(null);
+            } else {
+                if (parens > 0 || ml_string) {
+                    // If we have open parentheses or are in a """ string, use the previous line's indentation
+                    indents.push(indents[indents.length-1]);
+                } else {
+                    // Indent by the number of spaces at the start (accounts for tabs)
+                    indents.push(CodeMirror.countColumn(line.text, null, cm.getOption("tabSize")));
+                }
 
-				parens += (line.text.match(/\(|\[|\{/g) || []).length
-				parens -= (line.text.match(/\)|\]|\}/g) || []).length
-			}
-		});
-		for (var i = indents.length - 1; i >=0; --i) {
-			if (indents[i] === null) {
-				if (i == indents.length - 1)
-					indents[i] = 0;
-				else if (/^\s*(else|elif)\s*:/.test(cm.getLine(i+1)))
-					indents[i] = null
-				else
-					indents[i] = indents[i+1]
-			}
-		}
-		for (var i = 1; i < indents.length; ++i) {
-			if (indents[i] === null) {
-				indents[i] = indents[i-1]
-			}
-		}
+                // See (or rather, approximate) if there are any open parentheses
+                // or """ strings.
+                //
+                // This has lots of edge cases that won't work (in particular,
+                // stuff inside strings), but it's close enough without having to
+                // write a full python lexer.
+                var text = line.text.replace(/#.*/,"");
+                if (ml_string) {
+                    ml_string = ((text.match(/"""/g) || []).length % 2 === 0)
+                } else {
+                    ml_string = ((text.match(/"""/g) || []).length % 2 === 1)
+                }
+                if (!ml_string) {
+                    parens += (text.match(/\(|\[|\{/g) || []).length
+                    parens -= (text.match(/\)|\]|\}/g) || []).length
+                }
+            }
+        });
 
-		var scopeBoxes = [];
-		var curScopes = [0];
-		var curScopeBoxes = [];
-		cm.eachLine(function(line) {
-			var li = cm.lineInfo(line);
-			var indent = indents[li.line];
-			var curScope = curScopes[curScopes.length - 1];
-			//console.log(curScope);
-			if (indent > curScope) {
-				var coords = cm.charCoords({line:li.line, ch: indent}, "local");
-				var coords0 = cm.charCoords({line:0, ch: 0}, "local");
-				curScopes.push(indent);
-				curScopeBoxes.push({left: coords.left - coords0.left, top: coords.top - coords0.top, height: 0, level: curScopes.length-1});
-			} else {
-				while (indent < curScopes[curScopes.length - 1]) {
-					//console.log("popping");
-					scopeBoxes.push(curScopeBoxes.pop());
-					curScopes.pop();
-				}
-			}
-			curScopeBoxes.forEach(function(box) {
-				box.height += line.height;
-			});
-		});
-		while (curScopeBoxes.length) {
-			scopeBoxes.push(curScopeBoxes.pop());
-		}
+        // Go backwards through the indents, setting empty lines (with null
+        // indent) to have the next line's indent.
+        //
+        // Going backwards makes blocks end on their last line with content, as
+        // opposed to above the next line with content (which is what happens if
+        // you use the previous line's indent)
+        // 
+        // def foo():
+        //    pass <-- block ends here
+        //
+        //         <-- not here
+        // foo()
+        //
+        for (var i = indents.length - 1; i >=0; --i) {
+            if (indents[i] === null) {
+                if (i == indents.length - 1)
+                    indents[i] = 0;
+                else {
+                    // The exception is when the next block is paired with this
+                    // block, e.g. if/else, in which case we want to continue
+                    // the block to the end, done in the next pass.
+                    // 
+                    // FIXME: nested ifs continue to the previous nesting level's else
+                    var nextLine = cm.getLine(i+1);
 
-		scopeBoxes.sort(function(a,b) { return a.level - b.level; })
+                    // If the chopped off line starts with an else or whatever,
+                    // then we'll need to continue the block, so flip the sign.
+                    // Otherwise, just copy the next line's indentation.
+                    if (/^\s*(else|elif|except|finally)\s*:/.test(nextLine))
+                        indents[i] = null;
+                    else
+                        indents[i] = indents[i+1];
+                }
+            }
+        }
+        // Go back forward, setting any missing indents (from the first part of
+        // two-part blocks) with their predecessor's indentation
+        for (var i = 1; i < indents.length; ++i) {
+            if (indents[i] === null) {
+                indents[i] = indents[i-1];
+            }
+        }
 
-		var right = cm.display.scroller.offsetWidth;
-		scopeBoxes.forEach(function(box) {
-			var elt = document.createElement("div");
-			elt.className = "scope-bg scope-bg-" + box.level;
-			elt.style.position = "absolute";
-			elt.style.left = box.left + "px";
-			elt.style.top = box.top + "px";;
-			elt.style.height = box.height + "px";;
-			elt.style.width = (right - box.left) + "px";;
-			cm.display.lineSpace.insertBefore(elt, cm.display.cursorDiv);
-		});
-	}
+        // For each increase in indentation level, find blocks which start with
+        // that indent, and continue while indent >= start indent
+        var scopeBoxes = [];
+        var curScopes = [0];
+        var curScopeBoxes = [];
+        var line0 = cm.heightAtLine(0);
+        cm.eachLine(function(line) {
+            var li = cm.lineInfo(line);
+            var indent = indents[li.line];
+            var curScope = curScopes[curScopes.length - 1];
+            if (indent > curScope) {
+                // If the indentation level increased, that means we're starting a new box
+                curScopes.push(indent);
+                curScopeBoxes.push({left: cm.defaultCharWidth()*indent, top: cm.heightAtLine(li.line) - line0, level: curScopes.length-1});
+            } else {
+                // If the indentation level dropped, that means we're closing all boxes with higher indent level
+                while (indent < curScopes[curScopes.length - 1]) {
+                    var box = curScopeBoxes.pop();
+                    box.bottom = cm.heightAtLine(li.line) - line0;
+                    scopeBoxes.push(box);
+                    curScopes.pop();
+                }
+            }
+        });
+        // Close any left-over boxes
+        while (curScopeBoxes.length) {
+            var box = curScopeBoxes.pop();
+            box.bottom = cm.heightAtLine(cm.lastLine()+1) - line0;
+            scopeBoxes.push(box);
+        }
 
-	function refreshScopeBG(cm) {
-		clearScopeBG(cm);
-		setScopeBG(cm);
-	}
+        // Sort by increasing indent level so that higher level boxes are on
+        // top of lower indent ones
+        scopeBoxes.sort(function(a,b) { return a.level - b.level; })
+
+        // Add boxes to cm.display.lineSpace, behind text and cursors, stretching all the way to the right.
+        scopeBoxes.forEach(function(box) {
+            var elt = document.createElement("div");
+            elt.className = "cm-python-indent-guide cm-python-indent-guide-" + box.level;
+            elt.setAttribute("data-level", box.level);
+            elt.style.position = "absolute";
+            elt.style.left = box.left + "px";
+            elt.style.top = box.top + "px";
+            // Set bottom/right instead of width/height to compensate for styling on the indent guides, especially borders
+            elt.style.bottom = (cm.display.lineSpace.offsetHeight - box.bottom) + "px";
+            elt.style.right = "0px";
+            cm.display.lineSpace.insertBefore(elt, cm.display.cursorDiv);
+        });
+    }
+
+    function refreshPythonIndendationGuides(cm) {
+        clearPythonIndendationGuides(cm);
+        createPythonIndendationGuides(cm);
+    }
+
 
 	/* Directives */
 
@@ -125,7 +181,7 @@ define(["angular", "app/filters", "codemirrorPython"], function() {
 				lineNumbers: true,
 				theme: "neat",
 				tabSize: 2,
-				scopeBG: true,
+				pythonIndentationGuides: true,
 			    extraKeys: {
 			      Tab: function(cm) {
 			        if (cm.getSelection().length)
