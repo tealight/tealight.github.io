0197290e3958580a935c2364b27d08eaa65a528c
Ian Davies <git@iandavies.org>
2014-08-21 10:59:04 +0100

    Re-add github_load function. Partially fixes #6.

--- app/js/app/skulpt-modules/tealight/utils.js
+++ app/js/app/skulpt-modules/tealight/utils.js
@@ -22,6 +22,22 @@ var $builtinmodule = function(name)
 
     	return new Sk.builtin.nmber((Date.now() - startTime) / 1000.0)
     })
+
+    mod.github_load = new Sk.builtin.func(function(repo_owner, mode, file) {
+        var http = new XMLHttpRequest();
+        var url = "https://api.github.com/repos/" + repo_owner.v + "/tealight-files/contents/" + mode.v + "/" + file.v + ".py?access_token=" + params.githubToken;
+
+        http.open("GET", url, false);
+        http.send(null);
+
+        if (http.status == 200) {
+            var py = atob(JSON.parse(http.responseText).content.replace("\n", ""));
+
+            return Sk.importModuleInternal_(file.v, false, undefined, py);
+        } else {
+            throw new Sk.builtin.Exception("Could not load " + mode.v + "/" + file.v + " from Github user '" + repo_owner.v + "'");
+        }
+    })
 	
     return mod;
 }
